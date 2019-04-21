d3e83fb2cf1e195f9cb944ce9ff81c7644ff284d
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-21 03:31:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,10 +40,11 @@ def selective_tweet_shortener(tweet)
   end 
 end 
 
-def shortened_tweet_truncator
+def shortened_tweet_truncator(tweet)
     if word_substituter(tweet).chars.length >= 140
       truncated_tweet = word_substituter(tweet).chars[0..136].push
-      
+    else 
+      tweet
 
 
 
