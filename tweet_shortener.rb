09d7c25772c2d8818cca51821db5c274cfbd1bdd
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-21 02:51:37 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,44 @@
 # Write your code here.
+
+def dictionary
+  substitutes = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split(" ").map do | word |
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+
+def bulk_tweet_shortener(tweet)
+  tweet.map do | tweets |
+    puts word_substituter(tweets)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.split(" ").map do | tweets |
+    if tweets.length >140
+      word_substituter(tweets)
+    else
+      tweets
+    end
+  end
+end
+    
